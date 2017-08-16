a1cc78710404f305a6bd24709f35791fe4cc816b
mesutozturk <mesutoztrk@msn.com>
2017-08-16 17:34:37 +0300

    foursquare api

--- Section3/app/scripts/controllers/main.js
+++ Section3/app/scripts/controllers/main.js
@@ -8,9 +8,12 @@
  * Controller of the firstangularApp
  */
 app.controller('MainCtrl', function ($scope, api) {
+  $scope.firmalar = [];
+
   $scope.firmaara = function () {
-    api.searchVenue($scope.ara, function (response) {
-      console.log(response);
+    api.searchVenue($scope.ara, function (data) {
+      console.log(data.response.venues);
+      $scope.firmalar=data.response.venues;
     });
   };
 });
