b9a5177fcb36038fc52f4888ee3ef3003a4c7052
mesutozturk <mesutoztrk@msn.com>
2017-08-16 15:38:07 +0300

    angular

---
+++ Section3/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstangularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the firstangularApp
+ */
+angular.module('firstangularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
