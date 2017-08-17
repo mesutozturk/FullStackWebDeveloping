07f729c8a4fb627e64cae4cd9787524e52cc8035
mesutozturk <mesutoztrk@msn.com>
2017-08-17 15:46:35 +0300

    google direction api

--- Section3/app/scripts/controllers/detay.js
+++ Section3/app/scripts/controllers/detay.js
@@ -19,7 +19,7 @@ app.controller("DetayCtrl", function ($scope, $routeParams, api) {
             }
             haritadagoster();
         });
-        api.getphotos($routeParams.id,function(data){
+        api.getphotos($routeParams.id, function (data) {
             console.log(data);
         });
     }
@@ -46,6 +46,57 @@ app.controller("DetayCtrl", function ($scope, $routeParams, api) {
             title: $scope.firma.name,
             animation: google.maps.Animation.DROP
         });
+    }
+    
+    $scope.gotur = function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(goturbeni,function (err){
+                console.log(err);
+            });
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+    }
+    function goturbeni(konum){
+        localStorage.setItem("konum","enlem: "+konum.coords.latitude+" boylam:"+konum.coords.longitude);
+        localStorage.removeItem("konum");
+        var benimlatlng = {
+            lat: konum.coords.latitude,
+            lng: konum.coords.longitude
+        };
+        var gidileceklatlng = {
+            lat: $scope.firma.location.lat,
+            lng: $scope.firma.location.lng
+        }
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var directionsService = new google.maps.DirectionsService;
+        var mapdiv = document.getElementById('harita');
+        var settings = {
+            center:benimlatlng ,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            mapTypeControl: true,
+            navigationControlOptions: {
+                style: google.maps.NavigationControlStyle.SMALL
+            }
+        };
+        var map = new google.maps.Map(mapdiv, settings);
+        directionsDisplay.setMap(map);
+        directionsDisplay.setPanel(document.getElementById('yonlendirme'));
+        
+        directionsService.route({
+          origin: benimlatlng,
+          destination: gidileceklatlng,
+          travelMode: 'TRANSIT'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+        var trafficLayer = new google.maps.TrafficLayer();
+        trafficLayer.setMap(map);
     }
     getvenue();
 });
